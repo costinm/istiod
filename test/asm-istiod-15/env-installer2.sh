bb2325e2456ec0f93b6dbd8d2085a1e571d4948a
Costin Manolache <costin@gmail.com>
2020-02-07 16:44:07 -0800

    New test for istiod working on GCP

---
+++ test/asm-istiod-15/env-installer2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PROJECT_ID=costin-istio
+export CLUSTER_NAME=installer2
+export LOCATION=us-west1-b
---
+++ test/asm-istiod-15/env-istiod2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PROJECT_ID=costin-istio
+export CLUSTER_NAME=istiod2
+export LOCATION=us-central1-a
