7ed625aba0cbbab9db13e8e524c01a486e94ece1
costin <costin@gmail.com>
2020-07-27 09:09:05 -0700

    Add back the original experiment of tcp over http.

--- test/cr/istiod-gcp.sh
+++ test/cr/istiod-gcp.sh
@@ -7,6 +7,7 @@ if [[ -n ${PROJECT} ]]; then
   gcloud container clusters get-credentials ${CLUSTER} --zone ${ZONE} --project ${PROJECT}
 fi
 
+ISTIOD_PORT=${ISTIOD_PORT:-443}
 
 # Disable webhook config patching - manual configs used, proper DNS certs means no cert patching.
 export VALIDATION_WEBHOOK_CONFIG_NAME=
@@ -14,6 +15,9 @@ export INJECTION_WEBHOOK_CONFIG_NAME=
 
 # TODO: get secrets as well, maybe file from storage
 export DISABLE_LEADER_ELECTION=true
+
+export USE_TOKEN_FOR_CSR=true
+export USE_TOKEN_FOR_XDS=true
 
 # Disable the DNS-over-TLS server
 export DNS_ADDR=
@@ -29,4 +33,4 @@ echo Starting with: $*
 exec /usr/local/bin/pilot-discovery discovery \
    --httpsAddr OFF \
    --secureGRPCAddr OFF \
-   ${EXTRA_ARGS} $*
+   ${EXTRA_ARGS} ${LOG_ARGS} $*
