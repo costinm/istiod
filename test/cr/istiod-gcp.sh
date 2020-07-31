6ac24e77716c06ceb4ca2e41e95cfc0360da1874
costin <costin@gmail.com>
2020-07-31 09:12:10 -0700

    Remove the second instance

--- test/cr/istiod-gcp.sh
+++ test/cr/istiod-gcp.sh
@@ -29,8 +29,29 @@ else
   cat /etc/istio/config/mesh
 fi
 
-kubectl get ns
+kubectl get ns istio-system
+if [[ "$?" == "1" ]]; then
+  kubectl create ns istio-system
+  kubectl apply -k github.com/istio/istio/manifests/charts/base
+fi
+
+# Make sure the mutating webhook is installed, and prepare CRDs
+# This also 'warms' up the kubeconfig - otherwise gcloud will slow down startup of istiod.
+kubectl get mutatingwebhookconfiguration istiod-managed
+if [[ "$?" == "1" ]]; then
+  echo "Mutating webhook missing, initializing"
+  # TODO: include the charts in the image !
+  cat /var/lib/istio/inject/mutating_template.yaml | envsubst > /var/lib/istio/inject/mutating.yaml
+  cat /var/lib/istio/inject/mutating.yaml
+  kubectl apply -f /var/lib/istio/inject/mutating.yaml
+else
+  echo "Mutating webhook found"
+fi
 echo Starting with: $*
+
+
+# TODO: if injection template, injection values are present in the cluster, get them and use instead of the
+# built-in templates. Same for mesh config.
 
 exec /usr/local/bin/pilot-discovery discovery \
    --httpsAddr OFF \
